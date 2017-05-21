f9a3e7c995dc5c469591ca5edba1434fb0225726
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2017-05-21 19:27:40 +0300

    gui kivy iptal edildi.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import StringProperty
-
+from socket import *
 
 class AgHaritalama(Screen):
     def portTarama(self,ip_adres):
-        print("ip adresi : %s" %ip_adres)
-    pass
+        print ("Port Tarama Basladi %s".format(ip_adres))
+        hedefIP = ip_adres
+        ports = [22, 23, 25, 53, 80, 443, 3389] #yaygin portlar ftp, telnet, smtp, dns, http, https, rdp
+        for i in ports:
+            s = socket(AF_INET, SOCK_STREAM)
+            sonuc = s.connect_ex((hedefIP, i))
+            if(sonuc == 0) :
+                print ('Port %d: ACIK') % (i,)
+                s.close()
 
 class AgHaritalamaApp(App):
     ip = StringProperty(None)
     def build(self):
         return AgHaritalama()
-        
 
 if __name__ == "__main__":
     AgHaritalamaApp().run()
